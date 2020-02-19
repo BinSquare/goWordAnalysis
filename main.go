3149a76fa3d544f6152bfec1b89351d89ae9390e
BinBin He <hebinsquared@gmail.com>
2020-02-18 22:13:18 -0600

    Feature Update: Added in todo stemExtract function in main

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func stemExtract(word string) string {
+	return "test"
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
